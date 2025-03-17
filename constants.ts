d51bb2903ae71b644db21c4f4a7d54b05948a85c
Ihor Shchypakov <>
2025-03-17 21:10:32 +0100

    Init commit

--- constants.ts
+++ constants.ts
@@ -20,15 +20,15 @@ export const moveAnimationDuration = 150; // ms
 export const gameWinTileValue = Number(process.env.NEXT_PUBLIC_WIN_TILE_VALUE)||2048;
 
 export const gameTileImages: { [key: number]: string } = {
-  2: "/tiles/snake2.jpg",
-  4: "/tiles/dino4.jpg",
-  8: "/tiles/frog8.jpg",
-  16: "/tiles/ape16.jpg",
-  32: "/tiles/boar32.jpg",
-  64: "/tiles/whale64.jpg",
-  128: "/tiles/chad128.jpg",
-  256: "/tiles/piggy256.png",
-  512: "/tiles/piggy512.png",
-  1024: "/tiles/piggy1024.png",
-  2048: "/tiles/piggy2048.png",
+  2: "/tiles/2.jpg",
+  4: "/tiles/4.jpg",
+  8: "/tiles/8.jpg",
+  16: "/tiles/16.jpg",
+  32: "/tiles/32.jpg",
+  64: "/tiles/64.jpg",
+  128: "/tiles/128.jpg",
+  256: "/tiles/256.jpg",
+  512: "/tiles/512.jpg",
+  1024: "/tiles/1024.jpg",
+  2048: "/tiles/2048.jpg",
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,6 @@ export default function Home() {
       </div>
       <div>
         <SaveForm />
-        <GameSelector />
       </div>
       <footer>
         <Socials />
